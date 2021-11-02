091f6130f06b32e5cb00bd3b0b2b36e5375941a8
Bruno Martins <bruno.martins217@gmail.com>
2021-11-02 07:54:25 -0300

    desafio capitulo

---
+++ ProjectOne/src/classe/desafio/Jantar.java
@@ -0,0 +1,17 @@
+package classe.desafio;
+
+public class Jantar {
+  public static void main(String[] args) {
+    Comida c1 = new Comida("Arroz", 0.3);
+    Comida c2 = new Comida("Feijao", 0.3);
+
+    Pessoa p1 = new Pessoa("João", 75);
+
+    
+    System.out.println(p1.apresentar());
+    p1.comer(c1);
+    System.out.println(p1.apresentar());
+    p1.comer(c2);
+    System.out.println(p1.apresentar());
+  }
+}

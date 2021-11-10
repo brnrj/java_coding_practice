03ab4bdb88d041d7542811b7f6ddaedf3c567464
Bruno Martins <bruno.martins217@gmail.com>
2021-11-10 11:28:06 -0300

    modificadores de acesso

---
+++ ProjectOne/src/oo/encapsulamento/casaA/Ana.java
@@ -0,0 +1,10 @@
+package oo.encapsulamento.casaA;
+
+public class Ana {
+
+  @SuppressWarnings("unused")
+  private String segredo = "..."; // privado
+  String facoDentroDeCasa = "..."; //default ou package
+  protected String formaDeFalar = "..."; // 'é transmitido por herança'
+  public String todosSabem = "..."; // publico
+}

4670f5975a855836152de2a048c57a9d21f9daa3
Bruno Martins <bruno.martins217@gmail.com>
2021-11-10 22:31:15 -0300

    primieira funcao lambda

---
+++ ProjectOne/src/lambdas/CalculoTeste2.java
@@ -0,0 +1,13 @@
+package lambdas;
+
+public class CalculoTeste2 {
+  public static void main(String[] args) {
+    Calculo calcular = (x, y) -> { return x + y; };
+
+    System.out.println(calcular.executar(2, 3));
+
+    calcular = (x, y) -> x * y;
+
+    System.out.println(calcular.executar(2, 3));
+  }
+}
--- ProjectOne/src/oo/heranca/desafio/Luxo.java
+++ ProjectOne/src/oo/heranca/desafio/Luxo.java
@@ -3,6 +3,7 @@ package oo.heranca.desafio;
 public interface Luxo {
   void ligarAr();
   void desligarAr();
+  // Possibilidade de uma implementação padrão dentro de uma interface
   default int velocidadeDoAr() {
     return 1;
   }

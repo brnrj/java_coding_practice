4f8278a0b909c16ca990dbaebb64a0abf9de79c7
Bruno Martins <bruno.martins217@gmail.com>
2021-11-11 08:11:30 -0300

    interfaces funcionais#2

--- ProjectOne/src/lambdas/Calculo.java
+++ ProjectOne/src/lambdas/Calculo.java
@@ -1,7 +1,15 @@
 package lambdas;
 
-@FunctionalInterface
+@FunctionalInterface // permite apenas um método abstrato na interface
 public interface Calculo {
 
   public double executar(double a, double b);
+
+  default String legal() {
+    return "legal";
+  }
+
+  static String muitoLegal() {
+    return "Muito legal";
+  }
 }

2424daf1de87115cb26b6829d989a9db7fc522ed
Bruno Martins <bruno.martins217@gmail.com>
2021-11-17 09:50:19 -0300

    reduce#2

--- ProjectOne/src/streams/Aluno.java
+++ ProjectOne/src/streams/Aluno.java
@@ -3,10 +3,16 @@ package streams;
 public class Aluno {
   final String name;
   final double nota;
+  final boolean bomComportamento;
   
   public Aluno(String name, double nota) {
+    this(name, nota, true);
+  }
+
+  public Aluno(String name, double nota, boolean bomComportamento) {
     this.name = name;
     this.nota = nota;
+    this.bomComportamento = bomComportamento;
   }
   
 }
